c70319d7d97deac2c84a66114468fb40f0db4db4
matthewtranmer <matthew.tranmer@outlook.com>
2022-05-18 09:53:41 +0000

    Update

--- Server/main.go
+++ Server/main.go
@@ -186,10 +186,11 @@ type BoatTemplateData struct {
 type Boat struct {
 	Name  string
 	Image string
+	Price string
 }
 
 func (p *Pages) boats(w http.ResponseWriter, r *http.Request, user_details *handler.UserDetails) handler.ErrorResponse {
-	boat_storage := []Boat{{"Boat 0", "0"}, {"Boat 1", "1"}, {"Boat 2", "2"}, {"Boat 3", "3"}, {"Boat 4", "4"}, {"Boat 5", "5"}}
+	boat_storage := []Boat{{"Boat 0", "0", "£600"}, {"Boat 1", "1", "£1900"}, {"Boat 2", "2", "£1400"}, {"Boat 3", "3", "£6000"}, {"Boat 4", "4", "£1000"}, {"Boat 5", "5",  "£49,999"}}
 
 	err := p.executeTemplates(w, "boats.html", BoatTemplateData{user_details, boat_storage})
 	if err != nil {
